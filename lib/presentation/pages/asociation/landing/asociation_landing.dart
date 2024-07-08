0749e1ab460bb72e727a1a9c1195483a8b0322b0
andresjuarez1 <213364@ids.upchiapas.edu.mx>
2024-07-07 18:19:13 -0600

    select direction of my asociation

--- lib/presentation/pages/asociation/landing/asociation_landing.dart
+++ lib/presentation/pages/asociation/landing/asociation_landing.dart
@@ -1,3 +1,4 @@
+// association_landing_page.dart
 import 'package:flutter/material.dart';
 import 'components/navbar_asociation.dart';
 import 'components/active_events.dart';
@@ -9,6 +10,8 @@ import '../volunteers/volunteers_details.dart';
 import '../pastEvent/past_event.dart';
 import '../pastEventsPage/past_events_page.dart';
 import 'components/custom_drawer.dart';
+import '../map/select_location_page.dart';
+import '../map/show_location_page.dart';
 
 class AssociationLandingPage extends StatelessWidget {
   final List<Map<String, String>> imgList = [
@@ -101,6 +104,24 @@ class AssociationLandingPage extends StatelessWidget {
     print('Donación confirmada');
   }
 
+  void _navigateToSelectLocation(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SelectLocationPage(),
+      ),
+    );
+  }
+
+  void _navigateToShowLocation(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ShowLocationPage(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,7 +130,7 @@ class AssociationLandingPage extends StatelessWidget {
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(.0),
+          padding: const EdgeInsets.all(0.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
@@ -129,6 +150,17 @@ class AssociationLandingPage extends StatelessWidget {
                 navigateToPastEvent: _navigateToPastEvent,
                 navigateToAllPastEvents: _navigateToAllPastEvents,
               ),
+              
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => _navigateToSelectLocation(context),
+                child: const Text('Seleccionar Ubicación'),
+              ),
+              ElevatedButton(
+                onPressed: () => _navigateToShowLocation(context),
+                child: const Text('Mostrar Ubicación Guardada'),
+              ),
+
               SizedBox(height: 20),
               FooterComponent(
                 onDonateConfirmed: _onDonateConfirmed,
