eb44501b39ad75008e3cf6cc369f53b9a8ba4a1a
andresjuarez1 <213364@ids.upchiapas.edu.mx>
2024-07-16 14:30:14 -0600

    idk

--- lib/presentation/pages/register/registerEnterprise/map/select_location_manager.dart
+++ lib/presentation/pages/register/registerEnterprise/map/select_location_manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:geolocator/geolocator.dart';
 
 class SelectLocationPageManager extends StatefulWidget {
   @override
