34b6019505cb5e6118f21ae5d4f9b66896db7aa1
AylinMalpica09 <100156363+AylinMalpica09@users.noreply.github.com>
2024-07-23 12:58:44 -0600

    edit habilities

--- lib/data/mappers/volunteerProfile_mappers.dart
+++ lib/data/mappers/volunteerProfile_mappers.dart
@@ -10,7 +10,7 @@ ProfileVolunteerModel profileToProfileModel(VolunteerProfile profile) {
     email: profile.email,
     profilePicture: profile.profilePicture,
     id: profile.id,
-    text: profile.description,
+    description: profile.description,
   );
 }
 
@@ -23,6 +23,6 @@ VolunteerProfile profileModelToProfile(ProfileVolunteerModel profileModel) {
     email: profileModel.email,
     profilePicture: profileModel.profilePicture,
     id: profileModel.id,
-    text: profileModel.description
+      description: profileModel.description
   );
 }
